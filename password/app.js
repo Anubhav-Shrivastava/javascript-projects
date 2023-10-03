22ac7fd5235f9d4fb8387710d96aa4f652bd7358
Anubhav-Shrivastava <anubhavk282@gmail.com>
2023-10-03 20:35:40 +0530

    new

--- password/app.js
+++ password/app.js
@@ -1,13 +1,13 @@
 document.getElementById("passwordForm").addEventListener("submit", (event) => {
   event.preventDefault(); // Prevent the default form submission behavior
-  let pass = document.getElementById("password").value;
+  let pass = document.getElementById("password");
   let message=document.getElementById("info")
   let specialChars = "!@#$%^&*(?/";
   let hasSpecialChar = false;
 
 //  the code below checks for specialChars password is incude or note if its included its change the value of hasSpecialChar to tuue
   for (let char of specialChars) {
-      if (pass.includes(char)) {
+      if (pass.value.includes(char)) {
           hasSpecialChar = true;
           break;
       }
@@ -15,11 +15,30 @@ document.getElementById("passwordForm").addEventListener("submit", (event) => {
   // if the value of hasSpecialChar is ture then the following code is executed
 
 
-  if (hasSpecialChar) {
-    message.innerHTML=`password contain one of the following ${specialChars}`
-    message.style.color="green";
+  if (pass.value.length >= 4) {
+    if (hasSpecialChar === true) {
+      message.innerHTML = `Great! The password contains at least one of the following special characters: ${specialChars}`;
+      message.style.color = "green";
+    } else {
+      message.innerHTML = `Please use one of the following special characters: ${specialChars}`;
+      message.style.color = "red";
+    }
   } else {
-    message.innerHTML=`please use one of the folloeing ${specialChars}`
-    message.style.color="red"
+    message.innerHTML = "Password must be at least 4 characters long.";
+    message.style.color = "red";
   }
+
+  
+  let checkbox=document.getElementById("checkbox");
+  checkbox.addEventListener("click",()=>{
+      if(checkbox.checked){
+        pass.type="text"
+      }
+      else{
+        pass.type="password"
+      }
+
+  })
+
 });
+  
